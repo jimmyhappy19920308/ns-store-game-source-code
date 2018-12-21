457bd069358822122cdc01940673f84773f02222
yingming25 <karcnonf@gmail.com>
2018-12-21 17:57:38 +0800

    update to access state in template

--- src/components/ProductsPagination.vue
+++ src/components/ProductsPagination.vue
@@ -1,26 +1,26 @@
 <template>
   <div>
     <nav aria-label="Page navigation" class="my-5">
-      <ul class="pagination justify-content-center" v-if="parentPagination">
+      <ul class="pagination justify-content-center" v-if="$store.state['productsModules']['pagination']">
         <li
           class="page-item"
-          :class="{ disabled: parentPagination.has_pre === false }"
-          @click.prevent="getCurrentPage(parentPagination.current_page - 1)"
+          :class="{ disabled: $store.state['productsModules']['pagination'].has_pre === false }"
+          @click.prevent="getCurrentPage($store.state['productsModules']['pagination'].current_page - 1)"
         >
           <a class="page-link" href="#" tabindex="-1">Previous</a>
         </li>
         <li
           class="page-item"
-          :class="{ active: parentPagination.current_page === page }"
-          v-for="page in parentPagination.total_pages"
+          :class="{ active: $store.state['productsModules']['pagination'].current_page === page }"
+          v-for="page in $store.state['productsModules']['pagination'].total_pages"
           :key="page"
         >
           <a class="page-link" href="#" @click.prevent="getCurrentPage(page)">{{ page }}</a>
         </li>
         <li
           class="page-item"
-          :class="{ disabled: parentPagination.has_next === false }"
-          @click.prevent="getCurrentPage(parentPagination.current_page + 1)"
+          :class="{ disabled: $store.state['productsModules']['pagination'].has_next === false }"
+          @click.prevent="getCurrentPage($store.state['productsModules']['pagination'].current_page + 1)"
         >
           <a class="page-link" href="#">Next</a>
         </li>
