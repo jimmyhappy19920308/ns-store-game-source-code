f7e9740583d5d259a1a0db3b5127900409851558
yingming25 <karcnonf@gmail.com>
2018-12-21 11:52:30 +0800

    add actions's addCart method

--- src/store/carts.js
+++ src/store/carts.js
@@ -1,8 +1,33 @@
+import axios from 'axios';
+
 export default {
   namespaced: true,
   state: {
     shoppingCartCount: 0,
     carts: [],
     price: {},
+  },
+  actions: {
+    addCart(context, { id, qty }) {
+      const api = `${process.env.VUE_APP_API_PATH}/api/${process.env.VUE_APP_CUSTOM_PATH}/cart`;
+      const cart = {
+        product_id: id,
+        qty,
+      };
+
+      context.dispatch('updateLoading', true);
+
+      axios.post(api, { data: cart }).then(response => {
+        if (response.data.success) {
+          this.$bus.$emit('get-cart-count');
+
+          context.dispatch('updateLoading', false);
+
+          console.log(response.data.message);
+        } else {
+          console.log(response.data.message);
+        }
+      });
+    },
   },
 };
