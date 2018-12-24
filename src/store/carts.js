ff4a0ea7dcbee4ef754d31ab7175d3a99bfb7707
yingming25 <karcnonf@gmail.com>
2018-12-24 14:21:28 +0800

    update actions's method and add getters

--- src/store/carts.js
+++ src/store/carts.js
@@ -8,20 +8,27 @@ export default {
     price: {},
   },
   actions: {
-    addCart(context, { id, qty }) {
+    addToCart(context, { id, qty }) {
       const api = `${process.env.VUE_APP_API_PATH}/api/${process.env.VUE_APP_CUSTOM_PATH}/cart`;
+
+      if (qty === 0) {
+        context.dispatch('productsModules/setProductNum', 1, { root: true });
+        qty = 1;
+      }
+
       const cart = {
         product_id: id,
         qty,
       };
 
-      context.dispatch('updateLoading', true);
+      context.dispatch('updateLoading', true, { root: true });
 
       axios.post(api, { data: cart }).then(response => {
         if (response.data.success) {
-          this.$bus.$emit('get-cart-count');
+          // this.$bus.$emit('get-cart-count');
+          context.dispatch('getCart');
 
-          context.dispatch('updateLoading', false);
+          context.dispatch('updateLoading', false, { root: true });
 
           console.log(response.data.message);
         } else {
@@ -35,7 +42,7 @@ export default {
       axios.get(api).then(response => {
         if (response.data.success) {
           context.commit('CARTS', response.data.data.carts);
-          context.commit('PRICE', response.data.data.total);
+          context.commit('PRICE_TOTAL', response.data.data.total);
           context.commit('PRICE_FINAL_TOTAL', response.data.data.final_total);
           context.commit('CART_COUNT');
         }
@@ -54,6 +61,17 @@ export default {
     },
     CART_COUNT(state) {
       state.shoppingCartCount = state.carts.length;
+    },
+  },
+  getters: {
+    shoppingCartCount(state) {
+      return state.shoppingCartCount;
+    },
+    carts(state) {
+      return state.carts;
+    },
+    price(state) {
+      return state.price;
     },
   },
 };
