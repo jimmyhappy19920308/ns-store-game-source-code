e4f056587862f7efb402cedb939a888343fb3e9c
yingming25 <karcnonf@gmail.com>
2018-12-24 15:58:28 +0800

    add state's property's default value
    add cartsModules's state's property's default value

--- src/store/carts.js
+++ src/store/carts.js
@@ -4,8 +4,40 @@ export default {
   namespaced: true,
   state: {
     shoppingCartCount: 0,
-    carts: [],
-    price: {},
+    carts: [
+      {
+        coupon: {
+          code: 'testCode',
+          due_date: 6547658,
+          id: '-L9uIs5EfPibJpwwTMhN',
+          is_enabled: 1,
+          percent: 60,
+          title: '超級特惠價格',
+        },
+        final_total: 2160,
+        id: '-LATwxc_bIJu-AR4AlNj',
+        product: {
+          category: '衣服3',
+          content: '這是內容',
+          description: 'Sit down please 名設計師設計',
+          id: '-L9tH8jxVb2Ka_DYPwng',
+          image: 'test.testtest',
+          is_enabled: 1,
+          num: 1,
+          origin_price: 500,
+          price: 600,
+          title: '[賣]動物園造型衣服3',
+          unit: '個',
+        },
+        product_id: '-L9tH8jxVb2Ka_DYPwng',
+        qty: 6,
+        total: 3600,
+      },
+    ],
+    price: {
+      total: 3600,
+      final_total: 2160,
+    },
   },
   actions: {
     addToCart(context, { id, qty }) {
