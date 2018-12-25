e0e47ed9916344b3f15df99a079c7667c64bcb67
yingming25 <karcnonf@gmail.com>
2018-12-25 10:58:32 +0800

    update couponModules
    update and add actions's method,
    add state's property and getters's method for cart have no thing,
    add getters's method for coupon apply fail

--- src/store/coupon.js
+++ src/store/coupon.js
@@ -4,28 +4,29 @@ export default {
   namespaced: true,
   state: {
     coupon: '',
+    cartsAlert: '購物車尚未有任何商品',
     couponErrorMessage: '',
   },
   actions: {
-    applyCoupon(context) {
+    applyCoupon(context, couponCode) {
       const api = `${process.env.VUE_APP_API_PATH}/api/${process.env.VUE_APP_CUSTOM_PATH}/coupon`;
       const coupon = {
-        code: context.state.coupon,
+        code: couponCode,
       };
 
-      context.commit('updateLoading', true, { root: true });
+      context.dispatch('updateLoading', true, { root: true });
 
       axios.post(api, { data: coupon }).then(response => {
         if (response.data.success) {
-          context.dispatch('cartsModules/getCart', { root: true }); // 重新取得右下購物車的商品名稱、價格與數量
+          context.dispatch('cartsModules/getCart', null, { root: true }); // 重新取得右下購物車的商品名稱、價格與數量
 
-          context.commit('updateLoading', true, { root: false });
+          context.dispatch('updateLoading', false, { root: true });
 
           context.commit('ERROR_MESSAGE', '');
 
           console.log(response.data.message);
         } else {
-          context.commit('updateLoading', true, { root: false });
+          context.dispatch('updateLoading', false, { root: true });
 
           context.commit('ERROR_MESSAGE', response.data.message);
 
@@ -33,15 +34,27 @@ export default {
         }
       });
     },
+    updateCoupon(context, coupon) {
+      context.commit('UPDATE_COUPON', coupon);
+    },
   },
   mutations: {
     ERROR_MESSAGE(state, payload) {
       state.couponErrorMessage = payload;
     },
+    UPDATE_COUPON(state, coupon) {
+      state.coupon = coupon.trim();
+    },
   },
   getters: {
     coupon(state) {
       return state.coupon;
+    },
+    couponErrorMessage(state) {
+      return state.couponErrorMessage;
+    },
+    cartsAlert(state) {
+      return state.cartsAlert;
     },
   },
 };
