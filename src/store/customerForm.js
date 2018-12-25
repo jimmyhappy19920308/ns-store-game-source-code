38e0f77c60deeca5c747525162ce25873c185c5c
yingming25 <karcnonf@gmail.com>
2018-12-25 15:35:15 +0800

    add getters

--- src/store/customerForm.js
+++ src/store/customerForm.js
@@ -7,4 +7,21 @@ export default {
     address: '',
     message: '',
   },
+  getters: {
+    name(state) {
+      return state.name;
+    },
+    email(state) {
+      return state.email;
+    },
+    phone(state) {
+      return state.phone;
+    },
+    address(state) {
+      return state.address;
+    },
+    message(state) {
+      return state.message;
+    },
+  },
 };
