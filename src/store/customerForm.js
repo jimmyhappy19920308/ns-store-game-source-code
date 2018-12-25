a47b941b36eec35fe5f285d52bdc24361bacdc15
yingming25 <karcnonf@gmail.com>
2018-12-25 16:34:36 +0800

    add form handle's actions and mutaions

--- src/store/customerForm.js
+++ src/store/customerForm.js
@@ -7,6 +7,40 @@ export default {
     address: '',
     message: '',
   },
+  actions: {
+    updateName(context, name) {
+      context.commit('NAME', name);
+    },
+    updateEmail(context, email) {
+      context.commit('EMAIL', email);
+    },
+    updatePhone(context, phone) {
+      context.commit('PHONE', phone);
+    },
+    updateAddress(context, address) {
+      context.commit('ADDRESS', address);
+    },
+    updateMessage(context, message) {
+      context.commit('MESSAGE', message);
+    },
+  },
+  mutations: {
+    NAME(state, name) {
+      state.name = name;
+    },
+    EMAIL(state, email) {
+      state.email = email;
+    },
+    PHONE(state, phone) {
+      state.phone = phone;
+    },
+    ADDRESS(state, address) {
+      state.address = address;
+    },
+    MESSAGE(state, message) {
+      state.message = message;
+    },
+  },
   getters: {
     name(state) {
       return state.name;
