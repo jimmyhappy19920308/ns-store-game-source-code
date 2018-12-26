9a8b4ea328102a7db82ad37f0020b12253448620
yingming25 <karcnonf@gmail.com>
2018-12-26 10:47:45 +0800

    remove data to store's order module's state

--- src/components/CheckoutConfirmToPay.vue
+++ src/components/CheckoutConfirmToPay.vue
@@ -66,44 +66,6 @@ export default {
       type: String,
     },
   },
-  data() {
-    return {
-      order: {
-        create_at: 1523539834,
-        id: '-L9u2EUkQSoEmW7QzGLF',
-        is_paid: false,
-        message: '這是留言',
-        payment_method: 'credit_card',
-        products: {
-          LTWKjbtQsZSjerdwpBU: {
-            final_total: 999,
-            id: 'LTWKjbtQsZSjerdwpBA',
-            product: {
-              category: '',
-              content: '',
-              description: '',
-              id: '',
-              imageUrl: '',
-              price: 990,
-              title: '',
-              unit: '個',
-            },
-            product_id: 'LTWKjbtQsZSjerdwpBC',
-            qty: 1,
-            total: 999,
-          },
-        },
-        total: 100,
-        user: {
-          address: 'kaohsiung',
-          email: 'test@gmail.com',
-          name: 'test',
-          tel: '0912346768',
-        },
-      },
-      isLoading: false,
-    };
-  },
   methods: {
     getOrder() {
       const vm = this;
