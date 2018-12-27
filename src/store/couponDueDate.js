e160e4e56133a70539eb2d2f841b2deb6bacd024
yingming25 <karcnonf@gmail.com>
2018-12-27 14:48:40 +0800

    update state

--- src/store/couponDueDate.js
+++ src/store/couponDueDate.js
@@ -1,9 +1,7 @@
 export default {
   namespaced: true,
   state: {
-    nowDate: '',
-    dueDate: '',
-    timer: '6 Day 6 Hours 6 Minutes 6 Second',
+    dueDateTimer: '6 Day 6 Hours 6 Minutes 6 Second',
     time: 0,
   },
 };
