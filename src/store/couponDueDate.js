5fe04d2337d197ae8dfd1a5b625cb9ac8dfcabaf
yingming25 <karcnonf@gmail.com>
2018-12-27 15:30:51 +0800

    update state

--- src/store/couponDueDate.js
+++ src/store/couponDueDate.js
@@ -1,6 +1,8 @@
 export default {
   namespaced: true,
   state: {
+    nowDate: Math.floor(Date.now() / 1000),
+    dueDate: Math.round(new Date('2018-12-31T23:59:59Z').getTime() / 1000),
     dueDateTimer: '6 Day 6 Hours 6 Minutes 6 Second',
     time: 0,
   },
