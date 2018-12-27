9dd7e8175a695a461c2dd9b053a7eeec45f4ff40
yingming25 <karcnonf@gmail.com>
2018-12-27 15:45:29 +0800

    add actions for count duedate

--- src/store/couponDueDate.js
+++ src/store/couponDueDate.js
@@ -6,4 +6,22 @@ export default {
     dueDateTimer: '6 Day 6 Hours 6 Minutes 6 Second',
     remainTime: 0,
   },
+  actions: {
+    countDueDate(context) {
+      const remainTime = context.state.dueDate - context.state.nowDate - 28800;
+
+      const day = Math.floor(remainTime / (24 * 3600));
+      const hour = Math.floor((remainTime % (24 * 3600)) / 3600);
+      const minute = Math.floor((remainTime % 3600) / 60);
+      const second = Math.floor(remainTime % 60);
+
+      setInterval(() => {
+        context.commit('TIME', remainTime);
+        context.commit(
+          'DUE_DATE_TIMER',
+          `${day} Day ${hour} Hours ${minute} Minutes ${second} Seconds`,
+        );
+      }, 1000);
+    },
+  },
 };
