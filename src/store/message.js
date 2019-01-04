9ee51c78950e16a88c977939446109bc95681008
yingming25 <karcnonf@gmail.com>
2019-01-04 16:56:14 +0800

    add actions's method to remove message from array

--- src/store/message.js
+++ src/store/message.js
@@ -12,11 +12,25 @@ export default {
         status,
         timestamp,
       });
+
+      context.dispatch('REMOVE_MESSAGE_TIMING', timestamp);
+    },
+    removeMessageWithTiming(context, timestamp) {
+      setInterval(() => {
+        context.state.messages.forEach((item, index) => {
+          if (timestamp === item[index].timestamp) {
+            context.commit('REMOVE_MESSAGE_TIMING', index);
+          }
+        });
+      }, 3000);
     },
   },
   mutations: {
     MESSAGE(state, payload) {
       state.messages.push(payload);
+    },
+    REMOVE_MESSAGE_TIMING(state, index) {
+      state.messages.splice(index, 1);
     },
   },
   getters: {
