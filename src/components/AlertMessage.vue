c6f9af5fbc7fe99bff5bae01e88d18ef2d8f33c3
yingming25 <karcnonf@gmail.com>
2019-01-04 16:35:47 +0800

    add component

---
+++ src/components/AlertMessage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="message-alert">
+    <div class="alert alert-dismissible"
+      :class="'alert-' + item.status"
+      v-for="(item, index) in messages" :key="index">
+      {{ item.message }}
+      <button type="button" class="close" @click="removeMessage(index)" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters('messageModule', ['messages']),
+  },
+}
+</script>
