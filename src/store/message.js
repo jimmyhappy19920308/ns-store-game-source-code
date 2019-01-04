e1dae4adb22b36751cdb471e3550bf8592577d9c
yingming25 <karcnonf@gmail.com>
2019-01-04 16:48:14 +0800

    add actions's method to push message in array

--- src/store/message.js
+++ src/store/message.js
@@ -3,6 +3,22 @@ export default {
   state: {
     messages: [],
   },
+  actions: {
+    updateMessage(context, { message, status }) {
+      const timestamp = Math.floor(new Date() / 1000);
+
+      context.commit('MESSAGE', {
+        message,
+        status,
+        timestamp,
+      });
+    },
+  },
+  mutations: {
+    MESSAGE(state, payload) {
+      state.messages.push(payload);
+    },
+  },
   getters: {
     messages(state) {
       return state.messages;
